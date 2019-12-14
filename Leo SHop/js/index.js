3e80eee3c090001714d4d5c1a09a0e2260fb661c
DMovsumov <57489297+DMovsumov@users.noreply.github.com>
2019-12-14 21:24:34 +0500

    Доработан проект
    Доделана верстка, сделан адаптив + написан на чистом js своеобразный конвертер валют и перевод header блока сайта

--- Leo SHop/js/index.js
+++ Leo SHop/js/index.js
@@ -1,8 +1,14 @@
-const laguage = ['English', 'Russian', 'Spain']
+const laguage = ['English', 'Russian']
 const currency = ['USD', 'RUB', 'EUR']
+const languaguRussian = ['Аккаунт', 'Список желаний', 'Проверка', 'Войти', 'Зарегистрироваться', 'Главная', 'Женщинам', 'Мужчинам', 'Другое', 'Покупка', 'Корзина(0)']
 const select_laguage = document.querySelector('.language')
 const select_currency = document.querySelector('.currency')
 const item = document.createElement('option')
+
+let price = document.querySelectorAll('.price')
+
+let lang = document.querySelectorAll('.lang')
+
 
 function select() {
     for(let i = 0; i < laguage.length; i++){
@@ -17,5 +23,39 @@ function select() {
         select_currency.appendChild(item.cloneNode(true))
     }
 }
+
+
+select_currency.addEventListener('change', (e) => {
+        for(let per of price) {
+            if(e.target.value == "RUB"){
+                per.innerHTML = per.innerHTML * 63
+            }
+            else if(e.target.value == "EUR"){
+                per.innerHTML = per.innerHTML * 70 
+            }
+            else if (e.target.value == "USD") {
+                per.innerHTML = per.innerHTML 
+            }
+        }
+})
+
+let lang_english = []
+for(let i = 0; i < lang.length; i++){
+    lang_english[i] = lang[i].innerHTML
+}
+
+select_laguage.addEventListener('change', (e) => {
+    for(let i = 0; i < lang.length; i++){
+        if(e.target.value == "Russian"){
+            lang[i].innerHTML = languaguRussian[i]
+        }
+        if(e.target.value == "English"){
+            lang[i].innerHTML = lang_english[i]
+        }
+    }
+})
+
+
+   
 
 select()
